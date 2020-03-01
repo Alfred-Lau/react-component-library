8ae5844a6d80aaf0edd6ce269798bae156d8f4e0
Alfred-Lau <jian.liu@icloud.com>
2020-03-01 22:45:20 +0800

    add toolbox component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import TimelineData from './api/data';
 import Timeline from './components/TImeline';
+import ToolBox from './components/Toolbox';
 
 function App() {
     return (
         <div className='App'>
             <h1>React Timeline</h1>
             <Timeline data={TimelineData}></Timeline>
+            <ToolBox></ToolBox>
         </div>
     );
 }
---
+++ src/components/Toolbox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class ToolBox extends React.Component {
+    render() {
+        return <>我是toolbox组件</>;
+    }
+}
