8464cdbc697c9fddc14bdd3adeadab7e6da62eed
Alfred-Lau <jian.liu@icloud.com>
2020-03-13 08:05:47 +0800

    通过背景图片实现toolbox

--- src/components/Toolbox.js
+++ src/components/Toolbox.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import './toolbox.scss';
 
 export default class ToolBox extends React.Component {
-    render() {
-        return <>我是toolbox组件</>;
-    }
+  render() {
+    return (
+      <div className='toolbox-container'>
+        <div className='toolbox'>
+          <a className='toolbox-item toolbox-item-wechat' href='/#'>
+            <span className='toolbox-layer'></span>
+          </a>
+          <a className='toolbox-item toolbox-item-feedback' href='/#'>
+            <span></span>
+          </a>
+          <a className='toolbox-item toolbox-item-app' href='/#'>
+            <span className='toolbox-layer'></span>
+          </a>
+          <a className='toolbox-item toolbox-item-top' href='/#'>
+            <span></span>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
