a259f58af08fbdf9580b4cbfe4b51b033cea8470
Alfred-Lau <jian.liu@icloud.com>
2020-03-18 11:37:56 +0800

    调整组件布局

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import Timeline from './components/TImeline';
 import ToolBox from './components/Toolbox';
 
 function App() {
-    return (
-        <div className='App'>
-            <h1>React Timeline</h1>
-            <Timeline data={TimelineData}></Timeline>
-            <ToolBox></ToolBox>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <section className='item'>
+        <h1>React Timeline</h1>
+        <Timeline data={TimelineData}></Timeline>
+      </section>
+
+      <section className='item'>
+        <h1>React ToolBox</h1>
+        <ToolBox></ToolBox>
+      </section>
+    </div>
+  );
 }
 
 export default App;
